1bf5db93fca68b37d157ada04d0fb62cc9db22fe
SalmaMuntazShaumi <salmamumtazsoami@gmail.com>
2026-05-12 16:59:18 +0700

    [SM] feat edit profile

--- lib/repositories/users_repository.dart
+++ lib/repositories/users_repository.dart
@@ -5,6 +5,57 @@ import 'package:http/http.dart' as http;
 class UsersRepository {
   final String apiUrl = 'http://192.168.0.31:8000/';
 
+  Future<Map<String, dynamic>> updateProfile({
+    required String token,
+    required String name,
+    required String phone,
+    required String birthdate,
+    File? image,
+  }) async {
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('${apiUrl}api/profile/update'),
+      );
+
+      request.headers.addAll({
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      });
+
+      request.fields['name'] = name;
+      request.fields['phone'] = phone;
+      request.fields['birthdate'] = birthdate;
+
+      if (image != null) {
+        request.files.add(
+          await http.MultipartFile.fromPath(
+            'photo',
+            image.path,
+          ),
+        );
+      }
+
+      final streamedResponse = await request.send();
+
+      final response = await http.Response.fromStream(
+        streamedResponse,
+      );
+
+      final data = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        return data;
+      } else {
+        throw Exception(
+          data['message'] ?? 'Failed to update profile',
+        );
+      }
+    } catch (e) {
+      throw Exception('Update profile error: $e');
+    }
+  }
+
   fetchUserData(String token) async {
     try {
       final response = await http.get(
